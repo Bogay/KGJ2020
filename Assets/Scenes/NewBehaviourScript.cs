a95d628a6824fc06400dc041d8b0d5ef51c5d3ba
lai20021015 <lai2002101517@gmail.com>
2020-04-25 23:03:50 +0800

    bottom_prob
    bottom_prob

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Text counter;
+    public int count=0;
+    // Start is called before the first frame update
+    public void OnBtn0()
+    {
+        if (count > 9) count -= 10;
+        count++;
+        counter.text = "" + count;
+    }
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
